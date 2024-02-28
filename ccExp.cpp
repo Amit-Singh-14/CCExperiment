0cc52548114475c323091bc1bf989d3b4d90b8e7
Axcel014 <amitasdf246@gmail.com>
2024-02-28 11:34:05 +0530

    exp

---
+++ ccExp.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define show(n) cout<<(n)<<endl
+
+int main() {
+    cout << "Amit Singh 21BCS76" << endl;
+    cout << "Experiment 1.4" << endl;
+    return 0;
+}
