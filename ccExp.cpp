49d263e80ac4f298a494592f983ebd009d5768cf
Axcel014 <amitasdf246@gmail.com>
2024-02-28 11:35:50 +0530

    change

--- ccExp.cpp
+++ ccExp.cpp
@@ -4,7 +4,7 @@ typedef long long int ll;
 #define show(n) cout<<(n)<<endl
 
 int main() {
-    cout << "Amit Singh 21BCS76" << endl;
+    cout << "Amit Singh 21BCS5076" << endl;
     cout << "Experiment 1.4" << endl;
     return 0;
 }
